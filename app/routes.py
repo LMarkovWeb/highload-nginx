9e3d19ee7e077905ba730e1aaee328aca6cd615d
Leonid Markov <leonid.m@spayz.io>
2023-04-24 18:24:28 +0400

    add route with "/"

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,13 @@ factorial_routes = Blueprint('factorial_routes', __name__)
 io_routes = Blueprint('io_routes', __name__)
 
 @factorial_routes.route("/fac/<int:n>")
+@factorial_routes.route("/fac/<int:n>/")
 def get_factorial(n):
     fact = factorial(n)
     return jsonify({"factorial": fact})
 
 @io_routes.route("/io/<int:n>")
+@io_routes.route("/io/<int:n>/")
 def create_files_api(n):
     result = create_files(n)
     return jsonify({"Result": result})
