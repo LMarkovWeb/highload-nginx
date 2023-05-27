31e6ae152baf5d3adf4192f110080af1e282cd46
Maksim Suslov <m.suslov@tinkoff.ru>
2023-05-27 16:27:39 +0300

    Scale up app

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify
 from utils import factorial, create_files
+import socket
+
+hostname = socket.gethostname()
 
 factorial_routes = Blueprint('factorial_routes', __name__)
 io_routes = Blueprint('io_routes', __name__)
@@ -8,7 +11,7 @@ io_routes = Blueprint('io_routes', __name__)
 @factorial_routes.route("/fac/<int:n>/")
 def get_factorial(n):
     fact = factorial(n)
-    return jsonify({"factorial": fact})
+    return jsonify({"factorial": fact, "hostname": hostname})
 
 @io_routes.route("/io/<int:n>")
 @io_routes.route("/io/<int:n>/")
