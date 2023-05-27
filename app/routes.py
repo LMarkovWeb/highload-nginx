3d5e88ae5ce461511db8f12757084f977ec278c5
Maksim Suslov <m.suslov@tinkoff.ru>
2023-05-27 16:57:00 +0300

    Add different endpoints for tests

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from utils import factorial, create_files
 import socket
+import random
+import time
 
 hostname = socket.gethostname()
 
@@ -8,9 +10,24 @@ factorial_routes = Blueprint('factorial_routes', __name__)
 io_routes = Blueprint('io_routes', __name__)
 
 @factorial_routes.route("/fac/<int:n>")
-@factorial_routes.route("/fac/<int:n>/")
+@factorial_routes.route("/fac/<int:n>/")  # 1-100_000
 def get_factorial(n):
     fact = factorial(n)
+    return jsonify({"factorial": fact, "hostname": hostname})
+
+@factorial_routes.route("/fac/<int:n>")
+@factorial_routes.route("/fac/<int:n>/")  # 1-100_000
+def get_factorial_latency(n):
+    fact = factorial(n)
+    time.sleep(5)
+    return jsonify({"factorial": fact, "hostname": hostname})
+
+@factorial_routes.route("/fac/<int:n>")
+@factorial_routes.route("/fac/<int:n>/")  # 1-100_000
+def get_factorial_random_latency(n):
+    fact = factorial(n)
+    if random.random() <= 0.01:
+        time.sleep(1)
     return jsonify({"factorial": fact, "hostname": hostname})
 
 @io_routes.route("/io/<int:n>")
