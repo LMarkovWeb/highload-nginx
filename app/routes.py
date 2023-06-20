4f1de9208c2ad2245b2f4cda3eb32c78f08b26dc
Leonid Markov <lmarkov.webmaster@ya.ru>
2023-06-20 16:47:42 +0300

    add sleep and random sleep to functions

--- app/routes.py
+++ app/routes.py
@@ -9,20 +9,24 @@ hostname = socket.gethostname()
 factorial_routes = Blueprint('factorial_routes', __name__)
 
 @factorial_routes.route("/fac/<int:n>")
-@factorial_routes.route("/fac/<int:n>/")  # 1-100_000
+@factorial_routes.route("/fac/<int:n>/") 
 def get_factorial(n):
     fact = factorial(n)
     return jsonify({"factorial": fact, "hostname": hostname})
 
-@factorial_routes.route("/fac/<int:n>")
-@factorial_routes.route("/fac/<int:n>/")  # 1-100_000
+@factorial_routes.route("/facl/<int:n>")
+@factorial_routes.route("/facl/<int:n>/") 
+# Эмитация долгого выполения запроса. 
 def get_factorial_latency(n):
-    fact = factorial(n)
-    time.sleep(5)
+    fact = factorial(n) 
+    time.sleep(1) 
     return jsonify({"factorial": fact, "hostname": hostname})
 
-@factorial_routes.route("/fac/<int:n>")
-@factorial_routes.route("/fac/<int:n>/")  # 1-100_000
+# Эмитация долгого выполения запроса случайным образом
+# Генерируем случайное число в диапазоне от 0 до 1 и проверяем, 
+# если это число меньше или равно 0.01 (вероятность 1%), то задержка 1 сек.
+@factorial_routes.route("/faclr/<int:n>")
+@factorial_routes.route("/faclr/<int:n>/")  
 def get_factorial_random_latency(n):
     fact = factorial(n)
     if random.random() <= 0.01:
